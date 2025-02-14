dc66bdea9407f8efbe36a044adac449337699067
lester <tushartripathi2002@gmail.com>
2025-02-15 01:22:03 +0530

    Top button added & styled

--- scripts.js
+++ scripts.js
@@ -2,5 +2,22 @@ const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', () => {
-        navMenu.classList.toggle('show');
-    });
+    navMenu.classList.toggle('show');
+});
+
+// Scroll to Top Button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
